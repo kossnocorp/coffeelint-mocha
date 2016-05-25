7c9c5d466948b091f25c03ed9751ee3aa25bcc60
Sasha Koss <koss@nocorp.me>
2016-05-25 11:41:41 +0800

    Finalize package
    - Move rules to the root dir, so `coffeelint-mocha/no_skipped_tests`
      could be used in `coffeelint.json`.
    - Add docs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  'handle_done_callback': require('./src/handle_done_callback'),
-  'no_exclusive_tests': require('./src/no_exclusive_tests'),
-  'no_global_tests': require('./src/no_global_tests'),
-  'no_pending_tests': require('./src/no_pending_tests'),
-  'no_skipped_tests': require('./src/no_skipped_tests'),
-  'no_synchronous_tests': require('./src/no_synchronous_tests')
+  'handle_done_callback': require('./handle_done_callback'),
+  'no_exclusive_tests': require('./no_exclusive_tests'),
+  'no_global_tests': require('./no_global_tests'),
+  'no_pending_tests': require('./no_pending_tests'),
+  'no_skipped_tests': require('./no_skipped_tests'),
+  'no_synchronous_tests': require('./no_synchronous_tests')
 }
